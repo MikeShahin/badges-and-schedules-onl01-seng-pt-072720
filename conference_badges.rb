307cadd750685c213ab5d6d7439a9fc6daa78bf3
Mike Shahindoust <gobias101@gmail.com>
2020-08-03 23:23:51 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,10 +6,9 @@ end
 def batch_badge_creator(attendees)
   batch_badge_messages = []
   attendees.each do |names|
-    batch_badge_messages.push(
+    batch_badge_messages.push(badge_maker(names))
   end
   return batch_badge_messages
 end
 
-#batch_badge_creator["Edsger", "Ada", "Charles"]
-badge_maker("mike")
+batch_badge_creator["Edsger", "Ada", "Charles"]
